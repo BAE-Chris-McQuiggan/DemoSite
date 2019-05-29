22bdc514dff0198628ee2448a7736cfa3126d6f9
jm4clark <jamesmclark96@gmail.com>
2019-05-29 14:33:47 +0100

    Add new User to site

--- src/test/java/DemoSiteTests/Tests.java
+++ src/test/java/DemoSiteTests/Tests.java
@@ -1,5 +1,51 @@
 package DemoSiteTests;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Tests {
+
+	static WebDriver driver;
+
+	@BeforeClass
+	public static void setup() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\Administrator\\Downloads\\chromedriver_win32/chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+
+	@AfterClass
+	public static void tearDown() {
+		driver.quit();
+	}
+
+	@Test
+	public void test1() {
+		driver.navigate().to("http://thedemosite.co.uk/");
+		WebElement addUser = driver.findElement(
+				By.xpath("/html/body/div/center/table/tbody/tr[2]/td/div/center/table/tbody/tr/td[2]/p/small/a[3]"));
+		addUser.click();
+		WebElement userName = driver.findElement(By.xpath(
+				"/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[1]/td[2]/p/input"));
+		userName.sendKeys("My_User_Name");
+		WebElement passwordFeild = driver.findElement(By.xpath(
+				"/html/body/table/tbody/tr/td[1]/form/div/center/table/tbody/tr/td[1]/div/center/table/tbody/tr[2]/td[2]/p/input"));
+		passwordFeild.sendKeys("PassWord");
+		passwordFeild.submit();
+
+		WebElement previousUserAndPassword = driver
+				.findElement(By.xpath("/html/body/table/tbody/tr/td[1]/blockquote/blockquote[2]/blockquote"));
+		assertTrue(previousUserAndPassword.getText().contains("My_User_Name")
+				&& previousUserAndPassword.getText().contains("PassWord"));
+
+	}
 
 }
